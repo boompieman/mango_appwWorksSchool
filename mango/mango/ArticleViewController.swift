8a810db8d6e3be11108e28b1dfe29f0d24782f3d
王安妮 <pore0814@gmail.com>
2018-04-10 14:38:47 +0800

    save

--- mango/mango/ArticleViewController.swift
+++ mango/mango/ArticleViewController.swift
@@ -7,29 +7,53 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
 
 class ArticleViewController: UIViewController {
+    
+    var ref: DatabaseReference!
+    var articles :Article?
+    var author : Author?
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // print(Auth.auth().currentUser?.uid)
+        
+        
+      
+       
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func postArticle(_ sender: Any) {
+        let  id = ref.child("articls").childByAutoId().key
+        author = Author(id: (Auth.auth().currentUser?.uid)!, email: (Auth.auth().currentUser?.email)!)
+        articles = Article(id: id, title: "ios 標題", content: "ios 內容", tag:"ios 生活" , author: Author(id: (author?.id)!, email: (author?.email)!), createdTime: 20170814)
+       
+        
+        ref = Database.database().reference()
+        
+        
+        
+        let key = ref.child("articles").childByAutoId().key
+        let post = ["id": articles?.id,
+                    "content":articles?.content ,
+                    "creatTime": articles?.createdTime,
+                    "tag":articles?.tag ]
+                 
+     
+        ref.updateChildValues(childUpdates) as [String : Any]
+        
     }
-    */
+    
 
+    
+
+    
+    
+    
+    
+   
 }
