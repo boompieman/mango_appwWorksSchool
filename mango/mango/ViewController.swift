e341d3b64c47eabfe0da0d2fead77c30b44fc490
Boompieman <t0915290092@gmail.com>
2018-04-10 09:27:01 +0800

    init

---
+++ mango/mango/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mango
+//
+//  Created by MACBOOK on 2018/4/10.
+//  Copyright © 2018年 MACBOOK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
