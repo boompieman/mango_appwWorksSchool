cdd362646ac64d8bb14e29c6f077e20feb35c00e
王安妮 <pore0814@gmail.com>
2018-04-10 13:00:18 +0800

    create Arthor and Article Model

---
+++ mango/mango/Article.swift
@@ -0,0 +1,18 @@
+//
+//  Article.swift
+//  mango
+//
+//  Created by 王安妮 on 2018/4/10.
+//  Copyright © 2018年 MACBOOK. All rights reserved.
+//
+
+import Foundation
+
+struct  Article {
+    var id: String
+    var title: String
+    var content: String
+    var tag: String
+    var author: String
+    var created_time: Int
+}
--- mango/mango/FriendViewController.swift
+++ mango/mango/FriendViewController.swift
@@ -7,12 +7,13 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class FriendViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print(Auth.auth().currentUser?.uid)
         // Do any additional setup after loading the view.
     }
 
