feb3d92e620bdd0053dfb92169fdd023c083ccc5
王安妮 <pore0814@gmail.com>
2018-04-11 14:10:40 +0800

    finish tag and get article order by time

--- mango/mango/Article.swift
+++ mango/mango/Article.swift
@@ -8,11 +8,29 @@
 
 import Foundation
 
-struct  Article {
+struct Article {
     var id: String
     var title: String
     var content: String
     var tag: String
     var author: Author
     var createdTime: Int
+    
+    
+        
+
+    func getTime() -> String{
+        
+        let date = Date(timeIntervalSince1970: TimeInterval(self.createdTime/1000))
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMM dd YYYY hh:mm a"
+        let dateString =  dateFormatter.string(from: date)
+       
+        return dateString
+      
+       // let dateFormatter = DateFormatter()
+        
+        
+        
+    }
 }
