2a35e161353a6f6dbb6e6e30cd35a50cc4f8856e
王安妮 <pore0814@gmail.com>
2018-04-11 15:21:24 +0800

    finish  part5 and part 6

--- mango/mango/ArticleViewController.swift
+++ mango/mango/ArticleViewController.swift
@@ -15,7 +15,7 @@ class ArticleViewController: UIViewController {
     
     var ref: DatabaseReference?
     var getFriendId = [String]()
-    var tagAndTime = [Article]()
+   
     var id: String?
     var a: Article?
     var b: Article?
@@ -31,7 +31,7 @@ class ArticleViewController: UIViewController {
           ref = Database.database().reference()
         //  getFriendUserId()
        // print("getfriend",getFriendId)
-     findArticles(friendid: "rn5Zag6r74cYm4DFQrVO3CHRWWf1")
+     
     
     }
     
@@ -61,6 +61,9 @@ class ArticleViewController: UIViewController {
 
     }
     
+    
+    
+    
 //    func getFriendUserId()  {
 //        var friends = [String]()
 //
@@ -82,95 +85,105 @@ class ArticleViewController: UIViewController {
 //        }) { (error) in
 //            print(error.localizedDescription)
 //        }
-//    }
+    //    }
     
-    func findArticles(friendid:String) {
+    //get Firebase Data
+    func getArticle(articles: [String:AnyObject])-> [Article]{
+        var articlesArray=[Article]()
+        for article in articles {
+            if let eachArticle = article.value as? [String:AnyObject],
+                
+                let title = eachArticle["title"] as? String,
+                let content = eachArticle["content"] as? String,
+                let tag = eachArticle["tag"] as? String,
+                let author = eachArticle["author"] as? [String:AnyObject],
+                let authorId = author["id"] as? String,
+                let authorEmail = author["email"] as? String,
+                let createdTime = eachArticle["createdTime"] as? Int
+            {
+                
+                let a = Article(id: article.key, title: title, content:content , tag: tag, author:Author(id: authorId, email: authorEmail) , createdTime:createdTime)
+                
+                articlesArray.append(a)
+                //   print("articleArray",articlesArray)
+            }
+        }
+        return articlesArray
+    }
+
         
-        var articlesArray = [Article]() // 儲存比對後資料
+        
+
+    // 觀看特定好友名稱
+    @IBAction func searchFriendArticle(_ sender: Any) {
+        findArticles(friendid: "GkXFSHdMpccvKKjOjS4oAygVnjf1")
+        
+    }
+
+    func findArticles(friendid:String) {
+        // var articlesArray = [Article]() // 儲存比對後資料
         self.ref?.child("articles")
             .observeSingleEvent(of: .value, with: { (snapshot) in
                 // Get user value
-            if   let articles = snapshot.value as? [String:AnyObject] {
-                //  print(articles)
-    
-                
-                 for article in articles {
-                    if let eachArticle = article.value as? [String:AnyObject],
-            
-                        let title = eachArticle["title"] as? String,
-                        let content = eachArticle["content"] as? String,
-                        let tag = eachArticle["tag"] as? String,
-                        let author = eachArticle["author"] as? [String:AnyObject],
-                        let authorId = author["id"] as? String,
-                        let authorEmail = author["email"] as? String,
-                        let createdTime = eachArticle["createdTime"] as? Int
-                   {
-
-                        self.a = Article(id: article.key, title: title, content:content , tag: tag, author:Author(id: authorId, email: authorEmail) , createdTime:createdTime)
-
-                        if friendid == self.a!.author.id {
-                            articlesArray.append(self.a!)
-                         //   print("articleArray",articlesArray)
+                if let articlesFromFirebase = snapshot.value as? [String:AnyObject] {
+                    
+                    let articles = self.getArticle(articles: articlesFromFirebase)
+                    var filterdArticles = [Article]()
+                    for article in articles {
+                        if friendid == article.author.id {
+                            filterdArticles.append(article)
+                            print(filterdArticles)
                         }
-                   }
-                 }
-               }
+                    }
+                }
             }) { (error) in
                 print(error.localizedDescription)
         }
-        
-        
     }
     
 
     @IBAction func timeAndTab(_ sender: Any) {
-        self.ref?.child("articles")
+        self.ref?.child("articles").queryOrdered(byChild: "tag").queryEqual(toValue: "ios 生活")
             .observeSingleEvent(of: .value, with: { (snapshot) in
-          
-            // Get user value
-            if  let getartiles = snapshot.value as? [String:AnyObject]{
-           
-                for getarticle in getartiles {
-                    //var article = Article()
-                    guard let tag = getarticle.value["tag"] as? String  else{return}
 
-                    guard let title = getarticle.value["title"] as? String
-                        else { return}
-     
-                    guard let content = getarticle.value["content"] as? String else { return}
-
+                // Get user data
+                if  let getArtilesFromFirebase = snapshot.value as? [String:AnyObject]{
                     
-                    guard let  author = getarticle.value["author"] as? [String:AnyObject] else{return}
-                   // print(author)
-                    guard let authorId = author["id"] as? String else{return}
-                   // print(authorId)
-                    guard    let authorEmail = author["email"] as? String else{return}
-                   // print(authorEmail)
-                    guard let createdTime = getarticle.value["createdTime"] as? Int else { return}
                     
-                    self.a = Article(id: getarticle.key, title: title, content: content, tag: tag, author: Author(id: authorId, email: authorEmail), createdTime: createdTime)
-                    
-                    let date1 = Date(timeIntervalSince1970: TimeInterval(1523365200)) //2018年4月10日星期二 21:00:00
-                    let date2 = Date(timeIntervalSince1970: TimeInterval(1523368800)) //2018年4月10日星期二 22:00:00
-                    let articleDate = Date(timeIntervalSince1970: TimeInterval((self.a?.createdTime)!/1000))
-             
-                    
-                    if self.a?.tag == "ios 生活"  &&
-                        (articleDate > date1 &&  articleDate<date2)
-                        {
-                        print("time",self.a?.getTime())
-//                        print("--------")
-                        self.tagAndTime.append(self.a!)
-                        self.tagAndTime = self.tagAndTime.sorted(by: {$0.createdTime>$1.createdTime})
-                       print(self.tagAndTime)
+                    let data = self.getArticle(articles: getArtilesFromFirebase)
+                    var filterdArticles = [Article]()
+                    for article in data {
+                        let date1 = Date(timeIntervalSince1970: TimeInterval(1523365200)) //2018年4月10日星期二 21:00:00
+                        let date2 = Date(timeIntervalSince1970: TimeInterval(1523368800)) //2018年4月10日星期二 22:00:00
+                        let articleDate = Date(timeIntervalSince1970: TimeInterval((article.createdTime)/1000))
+                        var tagAndTime = [Article]()
+                        if article.tag == "ios 生活"  &&
+                            (articleDate > date1 &&  articleDate < date2){
+                            tagAndTime.append(article)
+                            tagAndTime = tagAndTime.sorted(by: {$0.createdTime>$1.createdTime})
+                            print(article.getTime())
+                            print(tagAndTime)
+                        }
+                        
                     }
-                  }
+                    
                 }
-         
+                
+      
          }) { (error) in
             print(error.localizedDescription)
         }
       }
+  
+    @IBAction func friendArticle(_ sender: Any) {
+        
+        
+        
+        
+    }
+    
+    
+    
     }
     
    
